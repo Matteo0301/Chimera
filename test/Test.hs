f60d3a732abcceab566522d6521ae142e2b54908
Matteo0301 <matteomariotti0301@gmail.com>
2023-10-21 14:57:00 +0000

    Deleted failing tests

--- test/Test.hs
+++ test/Test.hs
@@ -18,13 +18,9 @@ qcProps =
     testGroup
         "(checked by QuickCheck)"
         [ QC.testProperty "sort == sort . reverse" $
-            \list -> sort (list :: [Int]) == sort (reverse list)
-        , QC.testProperty "Fermat's little theorem" $
+            \list -> sort (list :: [Int]) == sort (reverse list),
+          QC.testProperty "Fermat's little theorem" $
             \x -> ((x :: Integer) ^ 7 - x) `mod` 7 == 0
-        , -- the following property does not hold
-          QC.testProperty "Fermat's last theorem" $
-            \x y z n ->
-                (n :: Integer) >= 3 QC.==> x ^ n + y ^ n /= (z ^ n :: Integer)
         ]
 
 unitTests =
@@ -32,7 +28,4 @@ unitTests =
         "Unit tests"
         [ testCase "List comparison (different length)" $
             [1, 2, 3] `compare` [1, 2] @?= GT
-        , -- the following test does not hold
-          testCase "List comparison (same length)" $
-            [1, 2, 3] `compare` [1, 2, 2] @?= LT
         ]
