c73ced55d45496241f40b0427f964d5be1ba748d
Matteo0301 <matteomariotti0301@gmail.com>
2023-10-22 08:58:01 +0000

    Added license headers

--- src/Library.hs
+++ src/Library.hs
@@ -1,3 +1,14 @@
+{-|
+Module      : Library
+Description : !!! INSERT MODULE SHORT DESCRIPTION !!!
+Copyright   : (c) 2023 Matteo Mariotti
+License     : GNU GPL v.3
+Maintainer  : matteomariotti0301@gmail.com
+Stability   : experimental
+Portability : POSIX
+
+!!! INSERT MODULE LONG DESCRIPTION !!!
+-}
 module Library
     (
     ) where
